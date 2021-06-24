7ed694918a759fe8926a4a0b0cdf25a72d1d00d8
mohamed abduallah <mohamed.ab96@hotmail.com>
2021-06-24 16:08:14 +0300

    add question page

--- js/animation.js
+++ js/animation.js
@@ -68,7 +68,121 @@ $(document).ready(
 
             startGridQueAni("grid-questions",100);//gird question animetion
             startGridQueAni("tags-reg",50);//gird question animetion
+            // custom select
+            $(".btn-select").click(function(e){
+                e.preventDefault();
+                $(this).next().css({
+                visibility:"visible"
+
+                });
+                if(!$(this).hasClass("btn-select-act"))
+                    $(this).toggleClass("btn-select-act");
+            }
+            );
+            $(window).click(function() {
+               const lists= document.querySelectorAll(".custom-select-list");
+               for(var i=0 ;i<lists.length;i++){
+                    if($(lists[i]).css("visibility")==="visible"){
+                        $(lists[i]).css({
+                            visibility:"hidden"
             
+                            });
+                    $(lists[i]).prev().toggleClass("btn-select-act");
+                            
+                    }
+                }
+            });
+            $(".custom-select").click(function(event){
+            event.stopPropagation();
+            });
+            $(".custom-select-item").click( function(){
+                    $(this).parent().prev().text($(this).text());
+                    $(this).parent().prev().append("<i class=\"fas fa-caret-down select-arrow\"></i>")
+                    $(this).parent().css({
+                        visibility:"hidden"
+        
+                        });
+                    $(this).parent().prev().toggleClass("btn-select-act");
+                }
+            );
+            // custom select end
+            //editor start
+            var tinyMDE = new TinyMDE.Editor({element: 'editor',content:" "});
+            var commandBar = new TinyMDE.CommandBar({element: 'editor-toolbar', editor: tinyMDE});
+            $(".tags-list").hide();
+            $("#que-tags-input").keydown(function (e) {
+
+                if(e.which==38||e.which==40)
+                    e.preventDefault();
+             })
+             var currentPos=0;
+            $("#que-tags-input").keyup(function (e) { 
+
+                if($(this).text().length==1)
+                    $(".current-list-tag").removeClass("current-list-tag");
+                
+                var  found=0;
+                var hasHover=null;
+                $(this).next().children().map(function(c){
+                    if(!$(this).text().startsWith($("#que-tags-input").val()))
+                        $(this).hide()
+                    else{
+                        found++;
+                        $(this).show();
+                    }
+                    if($(this).hasClass("current-list-tag"))
+                        hasHover=this;
+                });
+                if($(this).val()==""|| found==0)
+                $(this).next().css({display:"none"});
+                else
+                    $(this).next().css({display:"block"});
+                // console.log(e.which);
+                if(e.which==38){
+                    if(hasHover==null)
+                    $(this).next().children().last().addClass("current-list-tag");
+                else{
+                    $(hasHover).removeClass("current-list-tag");
+                    $(hasHover).prevAll("li:visible").first().addClass("current-list-tag");
+                currentPos--;
+                }
+                
+                                   
+                $(this).next().animate({
+                    scrollTop: "-=26"
+                  }, 200);
+            
+                }
+                else if(e.which==40){
+                    if(hasHover==null){
+                        $(this).next().children().first("li:visible").addClass("current-list-tag");
+                    }                    
+                    else{
+
+                        
+                        $(hasHover).removeClass("current-list-tag");
+                        $(hasHover).nextAll("li:visible").first().addClass("current-list-tag");
+    
+                    }
+                    currentPos++;
+                    console.log(currentPos);
+                    if(currentPos>found){
+                        currentPos=0;
+                                           
+                        $(this).next().animate({
+                            scrollTop: '0'
+                        }, 200);
+                        $(hasHover).removeClass("current-list-tag");
+                        $(this).next().children().first().addClass("current-list-tag");
+    
+                    }
+                    if(currentPos>=3)                    
+                        $(this).next().animate({
+                            scrollTop: '+=30'
+                        }, 200);
+                        }
+                    
+            });
         }
 
 );
